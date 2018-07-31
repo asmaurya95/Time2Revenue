24c5ad4c6791bcf9b9dacf56e3e1ea815fac5e3e
harshal306 <hrshlmittal306@gmail.com>
2018-07-31 22:54:44 +0530

    update the User Interface

---
+++ public/javascripts/newproject.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $("#project_details").click(function(){
+        $("#content1").slideToggle("slow");
+    });
+    
+    $("#ttr_details").click(function(){
+        $("#content2").slideToggle("slow");
+    });
+	
+	$("#effort_details").click(function(){
+        $("#content3").slideToggle("slow");
+    });
+	
+});
---
+++ public/javascripts/welcome.js
@@ -0,0 +1,5 @@
+// Get the Sidebar
+var mySidebar = document.getElementById("mySidebar");
+
+// Get the DIV with overlay effect
+var overlayBg = document.getElementById("myOverlay");
